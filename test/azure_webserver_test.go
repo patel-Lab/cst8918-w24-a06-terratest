e0738c2cd24a3212076a5a2bba04802bc4a5fedd
patel-Lab <pate0590@algonquinlive.com>
2024-02-16 20:55:13 -0500

    final changes

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-azure-subscription-id"
+var subscriptionID string = "3376488f-2d26-498b-ae49-4f7ca9b44cd2"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-id>",
+			"labelPrefix": "pate0590",
 		},
 	}
 
@@ -33,4 +33,44 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 
 	// Confirm VM exists
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+}
+
+func TestNICExistsAndConnectedToVM(t *testing.T) {
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../",
+		Vars: map[string]interface{}{
+			"labelPrefix": "dhuk0001",
+		},
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	vmName := terraform.Output(t, terraformOptions, "vm_name")
+	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
+
+	nicID := azure.GetNetworkInterfaceID(t, vmName, resourceGroupName, subscriptionID)
+	assert.NotNil(t, nicID, "Network Interface not found for VM")
+	assert.True(t, azure.NetworkInterfaceIsAttachedToVM(t, nicID, vmName, resourceGroupName, subscriptionID), "Network Interface is not attached to VM")
+}
+
+func TestCorrectUbuntuVersionRunningOnVM(t *testing.T) {
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../",
+		Vars: map[string]interface{}{
+			"labelPrefix": "dhuk0001",
+		},
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	vmName := terraform.Output(t, terraformOptions, "vm_name")
+	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
+
+	correctUbuntuVersion := "22_04-lts-gen2"
+	actualUbuntuVersion := azure.GetVirtualMachineOSVersion(t, vmName, resourceGroupName, subscriptionID)
+	assert.Equal(t, correctUbuntuVersion, actualUbuntuVersion, "Incorrect Ubuntu version running on the VM")
 }
